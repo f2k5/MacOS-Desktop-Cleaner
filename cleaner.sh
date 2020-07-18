5db644a873279f79e5693e4e5010cae15ab36098
fardinkhan <fardinkhan@uvic.ca>
2020-07-18 12:19:22 -0700

    pushed

---
+++ cleaner.sh
@@ -0,0 +1,13 @@
+mkdir "All Files"
+mv "Screen Shot"* "All Files"
+mv *png "All Files"
+mv *jpg "All Files"
+mv *jpeg "All Files"
+mv *pdf "All Files"
+cd "All Files"
+mkdir "Screenshots" "Other images" "PDF files"
+mv "Screen Shot"* Screenshots
+mv *png "Other images"
+mv *jpg "Other images"
+mv *jpeg "Other images"
+mv *pdf "PDF files"
