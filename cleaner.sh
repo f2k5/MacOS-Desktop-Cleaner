77ce1894f838501bea8f75bcbde77abf82b57bc2
fardinkhan <fardin_2510@hotmail.com>
2021-01-14 13:46:57 -0800

    added if conditions

--- cleaner.sh
+++ cleaner.sh
@@ -1,9 +1,15 @@
-mkdir "All Files"
+if [ ! -d "All Files" ]; 
+then
+	mkdir "All Files"
+fi
+ 
 mv "Screen Shot"* "All Files"
 mv *png "All Files"
 mv *jpg "All Files"
 mv *jpeg "All Files"
 mv *pdf "All Files"
+
+
 cd "All Files"
 mkdir "Screenshots" "Other images" "PDF files"
 mv "Screen Shot"* Screenshots
